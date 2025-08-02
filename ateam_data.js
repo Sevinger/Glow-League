981303bb4bb8db3ad814187cfd9e97c9cf3eefcd
Tomas Sevcik <sevciktomas312@gmail.com>
2025-08-02 20:36:53 +0100

    Přidány nové výsledky a sekce News

---
+++ ateam_data.js
@@ -0,0 +1,32 @@
+const aTeamData = {
+  "Jakub Daňo": {
+    "role": "Majitel",
+    "bio": ""
+  },
+  "Matyáš Holeček": {
+    "role": "Spolumajitel - Admin, Live Steward",
+    "bio": "Jsem Maty a momentálně v lize působím jako Admin, a i jako Live Steward. V lize jsem už nějaký ten pátek. Přesně jsem tu 2 dny od založení ligy, takže jsem OG. Od první až po třetí sezónu jsem byl jako jezdec týmu McLaren, kde jsem těch pár závodů vyhrál. Ve druhé sezóně jsem skončil na druhém místě v bodování. Třetí sezóna byla tragédie. Nejezdil jsem skoro top 5, spíše jsem začal být horší a horší. Čtvrtá sezóna byla jiná. Připojil jsem se k týmu Williamsu, kde bohužel jsem ukončil kariéru hned po dvou závodech, kvůli internetu. Po ukončení kariéry jsem se uvítal zpět do týmu McLaren jako Šéf týmu. S týmem jsem z posledního místa skončil na pátém místě. Tohle byla moje kariéra See ya."
+  },
+  "Jakub Mahr": {
+    "role": "Admin Team - Ikona ligy, Vítěz několika závodů v GRS",
+    "bio": ""
+  },
+  "Ondřej Zahálka": {
+    "role": "Admin Team",
+    "bio": ""
+  },
+  "Adam Suský": {
+    "role": "Admin Team",
+    "bio": ""
+  },
+  "Tobiáš Kubička": {
+    "role": "Admin Team",
+    "bio": ""
+  },
+  "Tomáš David Ševčík": {
+    "role": "Admin Team",
+    "bio": ""
+  }
+};
+
+
