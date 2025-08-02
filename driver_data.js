e11a30655ee5dd8ef1425356d20ecac5e15f1c89
Tomas Sevcik <sevciktomas312@gmail.com>
2025-08-02 20:07:43 +0100

    Přidány nové výsledky a sekce News

---
+++ driver_data.js
@@ -0,0 +1,102 @@
+const driverData = {
+  "Zahálka": {
+    "team": "RB",
+    "points": 211,
+    "tenure": "Sezóna 2"
+  },
+  "Loudil": {
+    "team": "Mercedes",
+    "points": 148,
+    "tenure": "Sezóna 2"
+  },
+  "Svat": {
+    "team": "Ferrari",
+    "points": 145,
+    "tenure": "Sezóna 3"
+  },
+  "Borko": {
+    "team": "RB",
+    "points": 122,
+    "tenure": "Sezóna 1"
+  },
+  "Neubauer": {
+    "team": "Red Bull",
+    "points": 89,
+    "tenure": "Sezóna 4"
+  },
+  "Drozd": {
+    "team": "Alpine",
+    "points": 69,
+    "tenure": "Sezóna 2"
+  },
+  "Vaňko": {
+    "team": "Mercedes",
+    "points": 68,
+    "tenure": "Sezóna 4"
+  },
+  "Korkobec": {
+    "team": "Aston Martin",
+    "points": 62,
+    "tenure": "Sezóna 3"
+  },
+  "Náhlovský": {
+    "team": "Mercedes",
+    "points": 60,
+    "tenure": "Sezóna 4"
+  },
+  "Beran": {
+    "team": "Red Bull",
+    "points": 57,
+    "tenure": "Sezóna 3"
+  },
+  "Košek": {
+    "team": "McLaren",
+    "points": 57,
+    "tenure": "Sezóna 4"
+  },
+  "Čermák": {
+    "team": "Alpine",
+    "points": 53,
+    "tenure": "Sezóna 2"
+  },
+  "Nic": {
+    "team": "Aston Martin",
+    "points": 52,
+    "tenure": "Sezóna 4"
+  },
+  "Valášek": {
+    "team": "Ferrari",
+    "points": 47,
+    "tenure": "Sezóna 2"
+  },
+  "Jirčík": {
+    "team": "Haas",
+    "points": 42,
+    "tenure": "Sezóna 2"
+  },
+  "Jarschel": {
+    "team": "Sauber",
+    "points": 36,
+    "tenure": "Sezóna 3"
+  },
+  "Petřík": {
+    "team": "McLaren",
+    "points": 34,
+    "tenure": "Sezóna 1"
+  },
+  "Ondrejka": {
+    "team": "Rezerva",
+    "points": 33,
+    "tenure": "Sezóna 4"
+  },
+  "Kubel": {
+    "team": "Rezerva",
+    "points": 25,
+    "tenure": "Sezóna 4"
+  },
+  "Stašík": {
+    "team": "Williams",
+    "points": 25,
+    "tenure": "Sezóna 4"
+  }
+};
